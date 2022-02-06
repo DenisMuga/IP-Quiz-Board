c22d2939e12ad10b96a53c3e8758ff26a8fb639a
DenisMuga <denismugah5@gmail.com>
2022-02-06 11:47:58 +0300

    chore: creating variables - created more variables q2, q3, q4, and q5 to access values in the html file

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 function check(){
     var c=0;
     var q1=document.questions.question1.value;
+    var q2=document.questions.question2.value;
+    var q3=document.questions.question3.value;
+    var q4=document.questions.question4.value;
+    var q5=document.questions.question5.value;
     if(q1=="Float"){c+=20}
+    if(q2=="Scripting"){c+=20}
+    if(q3=="Methods/Functions"){c+=20}
+    if(q4=="script"){c+=20}
+    if(q5=="italic()"){c+=20}
     document.write(c);
 }
